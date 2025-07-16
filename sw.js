a823b51b49669d6512af2b72b225e3be1e6e6262
Damian708 <swiniuch.damian@o2.pl>
2025-07-16 19:31:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,65 +1,26 @@
-const CACHE_NAME = 'salon;
-const staticAssets = [
-  '/salonwik/',
-  '/salonwik/index.html',
-  '/salonwik/manifest.json',
-  '/salonwik/icon-128x128.png',
-  '/salonwik/icon-144x144.png',
-  '/salonwik/icon-152x152.png',
-  '/salonwik/icon-180x180.png',
-  '/salonwik/icon-256x256.png',
-  '/salonwik/icon-384x384.png',
-  '/salonwik/icon-512x512.png'
+const CACHE_NAME = 'salon-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/style.css',
+  '/app.js',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-
-        const fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then((response) => {
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
-
-          const responseToCache = response.clone();
-
-          caches.open(CACHE_NAME)
-            .then((cache) => {
-              cache.put(event.request, responseToCache);
-            });
-
-          return response;
-        });
-      })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
